bd87b331a4e103a2e2aca51cf5012e2792d0469b
Dylan <dgrozdanich@gmail.com>
2017-02-08 13:07:17 -0800

    Added PictureViewController. So far, everything is working as intended.

---
+++ Snapchat-clone-practice/PictureViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PictureViewController.swift
+//  Snapchat-clone-practice
+//
+//  Created by Dylan on 2/8/17.
+//  Copyright © 2017 Dylan. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+
+}
