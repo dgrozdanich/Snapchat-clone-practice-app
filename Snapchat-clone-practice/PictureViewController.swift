430b47b66031648538971eafa123640f2bf3b586
Dylan <dgrozdanich@gmail.com>
2017-02-08 14:22:54 -0800

    added view controller + working camera

--- Snapchat-clone-practice/PictureViewController.swift
+++ Snapchat-clone-practice/PictureViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -16,14 +16,32 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+         imagePicker.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
 
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        // change to .camera after testing 
+        
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
